196398b715b54ef12266c54a853d1d1cf45830b3
Bynaryman <i.f.lledoux@gmail.com>
2026-07-05 20:44:02 +0200

    Fix deployment for generated course assets

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,5 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
-  skippedContentGlobs: ["_site/assets/**/*.html"],
+  skippedContentGlobs: ["_site/assets/**/*.html", "_site/assets/courses/**"],
 };
